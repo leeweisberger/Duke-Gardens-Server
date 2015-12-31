609a8fdc7aee6b7192215b375906dc40057f416f
Lee Weisberger <lee.weisberger@duke.edu>
2015-12-31 22:44:29 +0000

    all images added and pois all seem to be working

---
+++ routes/pois.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('mongodb://root:bitnami@localhost/admin/');
+db.on('open', function () { console.log("opened"); });
+db.on('error', function (err) { console.log("error connecting " + err); });
+
+db.once('open', function() {
+	console.log("ready to do some db stufffff!");
+    var poiSchema = new mongoose.Schema({
+        name: String,
+        prefix: String,
+        lat: Number,
+        lng: Number,
+        description: String,
+        summary: String,
+        category: String,
+        location: String,
+        image: String
+    
+    });
+    var Poi = db.model('Poi', poiSchema);
+
+    Poi.find({}, function(err, pois){
+        if(err){
+            console.log(err.errors);
+        }
+        else{
+            console.log("closing db");
+            mongoose.connection.close();
+  	    res.json(pois);
+        }
+    });
+
+});
+
+});
+
+module.exports = router;
